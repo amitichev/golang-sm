60c8c51a8d08c1bcc0002f92edd4de1aa58000ca
Michael Filonenko <filonenko.mikhail@gmail.com>
2021-12-22 15:09:24 +0300

    PoC

--- cmd/src/main.go
+++ cmd/src/main.go
@@ -8,6 +8,87 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+type RequestStruct struct {
+	Val int
+}
+
+func readFromTarantool() {
+	id, err := unix.SysvShmGet(2021, 1024, unix.IPC_CREAT|0o644)
+	if err != nil {
+		fmt.Println("Confusion with shmem", err)
+	}
+	b, err := unix.SysvShmAttach(id, 0, 0)
+	if err != nil {
+		fmt.Println("Confusion with shmat", err)
+	}
+	b[0] = 0
+	start := time.Now()
+	for {
+		for b[0] == 0 {
+			//time.Sleep(time.Microsecond)
+		}
+		if b[0] == 2 {
+			fmt.Println("END ")
+			break
+		}
+		var data RequestStruct
+		if err := msgpack.Unmarshal(b[1:], &data); err != nil {
+			fmt.Println("Error", err)
+			break
+		}
+
+		b[0] = 0
+	}
+	fmt.Println(time.Since(start))
+}
+
+func writeToTarantool() {
+	id, err := unix.SysvShmGet(1599, 1024, unix.IPC_CREAT|0o644)
+	if err != nil {
+		fmt.Println("Confusion with shmem", err)
+	}
+	b, err := unix.SysvShmAttach(id, 0, 0)
+	if err != nil {
+		fmt.Println("Confusion with shmat", err)
+	}
+	start := time.Now()
+	size := int(1e5)
+	var data []byte
+	var prevint int
+	for i := 0; i != size; i++ {
+		var k = 0
+		for b[0] != 0 {
+			//time.Sleep(time.Microsecond)
+			k = k + 1
+			if k == 10000 {
+
+				var tmp RequestStruct
+				msgpack.Unmarshal(data, &tmp)
+				fmt.Println(tmp, prevint)
+			}
+		}
+
+		prevint = i
+		data, _ = msgpack.Marshal(&RequestStruct{Val: i})
+		copy(b[1:], data)
+		// wait for data written right
+		b[0] = 1 // set 1, tnt set 0
+	}
+	fmt.Println(time.Since(start), size/int(time.Since(start).Milliseconds()))
+
+	data, err = msgpack.Marshal(nil)
+	if err != nil {
+		fmt.Errorf("marshal nil error %v", err)
+	}
+	for b[0] != 0 {
+		time.Sleep(time.Microsecond)
+	}
+	copy(b[1:], data)
+	b[0] = 1
+
+	fmt.Println(id)
+}
+
 func main() {
 
 	// opts := tarantool.Opts{User: "user", Pass: "password"}
@@ -21,30 +102,6 @@ func main() {
 	// }
 	// fmt.Println(time.Since(start))
 
-	id, err := unix.SysvShmGet(1499, 1024, unix.IPC_CREAT|0o644)
-	if err != nil {
-		fmt.Println("Confusion with shmem", err)
-	}
-	b, err := unix.SysvShmAttach(id, 0, 0)
-	if err != nil {
-		fmt.Println("Confusion with shmat", err)
-	}
-	type RequestStruct struct {
-		Val uint
-	}
-	start := time.Now()
-	for i := 0; i != 1e5; i++ {
-		for {
-			if b[0] == 0 {
-				data, _ := msgpack.Marshal(&RequestStruct{Val: uint(1e5 + 1 + i)})
-				copy(b, data)
-				break
-			}
-
-		}
-	}
-	fmt.Println(time.Since(start))
-
-	fmt.Println(id)
-
+	writeToTarantool()
+	readFromTarantool()
 }
--- test.lua
+++ test.lua
@@ -2,6 +2,7 @@ local fiber = require('fiber')
 local ffi=require'ffi'
 local msgpack=require('msgpack')
 local clock = require('clock')
+local log = require('log')
 ffi.cdef[[
     int shmget(size_t key, size_t size, int shmflg);
     void *shmat(int shmid, const void *shmaddr, int shmflg);
@@ -9,8 +10,7 @@ ffi.cdef[[
 ]]
 
 box.cfg{
-    listen = "/home/amitichev/code/tarantool-test/test.socket",
-
+    --listen = "/home/amitichev/code/tarantool-test/test.socket",
 }
 
 box.schema.create_space('test', {if_not_exists=true})
@@ -25,22 +25,60 @@ box.schema.user.create('user', {password='password', if_not_exists=true})
 box.schema.user.grant('user', 'write,read', 'space', 'test' , {if_not_exists=true})
 box.schema.user.grant('user', 'write,read', 'space', 'config' , {if_not_exists=true})
 
-
+box.space["test"]:truncate()
+box.snapshot()
 
 local function getdata()
-    local sm_id = ffi.C.shmget(1499, 1024, 932)
+    jit.off(true)
+    local sm_id = ffi.C.shmget(1599, 1024, 932)
     local sm_data = ffi.cast("char*", ffi.C.shmat(sm_id, ffi.cast("void*", 0), 0))
+    sm_data[0] = 0
     while true do
-        local data = msgpack.decode(sm_data, 1024)
+        while sm_data[0] == 0 do
+            fiber.yield()
+        end
+        assert(sm_data[0] == 1, tostring(sm_data[0]))
+        assert(sm_data[0] == 1, tostring(sm_data[0]))
+        
+        local data, err = msgpack.decode(sm_data+1, 1023)
+        assert(sm_data[0] == 1, tostring(sm_data[0]))
+        assert(sm_data[0] == 1, tostring(sm_data[0]))
+        if data == box.NULL then
+            print('data ', 'nil')
+            break
+        end
         if type(data) == "table" then
             box.space["test"]:insert{data.Val}
+            sm_data[0] = 0
         end
-        ffi.C.memset(sm_data, 0, 1024)
-        fiber.yield()
-
     end
+    print('---', box.space["test"]:len(), i)
 end
 
+
+local function senddata()
+    local sm_id = ffi.C.shmget(2021, 1024, 932)
+    local sm_data = ffi.cast("char*", ffi.C.shmat(sm_id, ffi.cast("void*", 0), 0))
+    
+    for _, tuple in box.space["test"]:pairs() do
+        while sm_data[0] ~= 0 do 
+            fiber.yield()
+        end
+        local data = msgpack.encode({['Val']=tuple[1]})
+        ffi.copy(sm_data+1, data) 
+        sm_data[0] = 1
+    end
+
+    print('all done')
+
+    while sm_data[0] == 1 do 
+        fiber.sleep(0.01)
+    end
+    local data = msgpack.encode(box.NULL)
+    
+    ffi.copy(sm_data+1, data) 
+    sm_data[0] = 2
+end
 
 -- start = clock.monotonic()
 -- for i = 1,1e5 do
@@ -49,4 +87,8 @@ end
 -- print(clock.monotonic() - start)
 -- box.space.test:truncate()
 
-fiber.new(getdata)
+print(box.space["test"]:len())
+getdata()
+print('GetData end')
+senddata()
+print('SendData end')
